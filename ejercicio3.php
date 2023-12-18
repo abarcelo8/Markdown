31c37f4196ea4a42899fd2aee051b06df1bfc7f0
abarcelo8 <145791019+abarcelo8@users.noreply.github.com>
2023-12-18 19:42:26 +0100

    .

---
+++ ejercicio3.php
@@ -0,0 +1,22 @@
+<?php
+$edad = $_Get['edad'];
+if ($edad > 0 && $edad < 3)
+{
+print "niño"!;
+}
+else
+if ($edad > 4 && $edad >17)
+{
+print "adolescente"!;
+}
+else
+if ($edad >18 && >64)
+{
+print "Adulto"!;
+}
+else
+if ($edad <64)
+{
+print "abuelo"!;
+}
+?>
