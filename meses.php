545f308816cc4b71a00b903fc2cd4cedbf300cdb
abarcelo8 <145791019+abarcelo8@users.noreply.github.com>
2023-12-18 19:36:11 +0100

    .

---
+++ meses.php
@@ -0,0 +1,7 @@
+<?php
+$months=array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+for($i=0; $i<12; $i++) {
+print $months[$i];
+print "<br>";
+}
+?>
